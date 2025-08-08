6831c88177497171c806ef052e8adeafffcc1c2e
Chirag Yadav <chirag.cmy@gmail.com>
2025-08-08 11:39:38 +0000

    modified:   .devcontainer/devcontainer.json 	new file:   Basic/datatypes.js

---
+++ Basic/datatypes.js
@@ -0,0 +1,11 @@
+//"use strict"; // treat all js code as latest version
+//alert(3+3) got alert beacuse we are using node js
+//number
+//bigint => when want to use large numbers
+//string
+//boolean => trur or falswe
+//null => standalone value
+//undefined 
+//symbol => unique
+
+ console.log(3+3) 
