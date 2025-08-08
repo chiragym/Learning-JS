48e5b32c92fed4d7b81cac76b3f0e382f065fb33
Chirag Yadav <chirag.cmy@gmail.com>
2025-08-08 14:01:28 +0000

    just chsnges

---
+++ Basic/summary.js
@@ -0,0 +1,11 @@
+//Primitave Datatype(call by value) = String,Number,boolean,null,undefined,Symbol,BigInt
+
+// non primitave datatype(call by reference) = Array, objects, functions
+
+//Symbol is sed for give uniqueness
+// const id = Symbol('123')
+// const anotherID = Symbol('123')
+// console.log(id === anotherID);
+
+//https://262.ecma-international.org/5.1/#sec-11.4.3
+
