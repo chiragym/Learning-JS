f366f6534ec9af430fd2de5f7a3850fd1fd0f965
Chirag Yadav <chirag.cmy@gmail.com>
2025-08-08 13:37:32 +0000

    conversion

--- Basic/convop.js
+++ Basic/convop.js
@@ -20,4 +20,21 @@
     // "name" = true
 
 // ************* Operations ********************
+// let value = 3
+// let negvalue = -value
+// console.log(negvalue)
+
+// let str1 ="Hello"
+// let str2 =" Chirag"
+// let str3 = str1 + str2
+// console.log(str3)
+// console.log("1" + 2)
+// console.log(1 + "2")
+// console.log("1" + "2")
+// console.log("1" + 2 + 2)
+// console.log(1 + 2 + "2")
+// console.log(true)
+// console.log(+true)
+
+
 
