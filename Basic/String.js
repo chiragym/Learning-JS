4238bfb55609d96498702ff2b7ae9a6956bfab40
Chirag Yadav <chirag.cmy@gmail.com>
2025-08-12 09:00:05 +0000

    Strings

---
+++ Basic/String.js
@@ -0,0 +1,30 @@
+// const fname = "Chirag"
+// const city = "Ahmedabad"
+
+// vaild but old method
+ // console.log(fname + lname );
+
+ //console.log(`Our winner is ${fname} from ${city}`);
+
+ const gameName = new String("Football")
+ const gameNam = new String("Football-game-new")
+
+ console.log(gameName.toUpperCase());
+ console.log(gameName.length);
+ console.log(gameName.charAt(2));
+ console.log(gameName.indexOf('t'));
+
+ const newString = "    Chirag    "
+ console.log(newString);
+ console.log(newString.trim());
+
+const url = "https://chirag.com/chirag%20Yadav"
+console.log(url.replace('%2o', '-'));
+console.log(url.includes('chirag'))
+
+console.log(gameNam.split('-'));
+
+ 
+ 
+ 
+ 
