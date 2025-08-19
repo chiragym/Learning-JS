4fbc21074225b43a53d70f5ed9bbe133f6a05789
Chirag Yadav <chirag.cmy@gmail.com>
2025-08-19 05:52:06 +0000

    nums

---
+++ Basic/nums_math.js
@@ -0,0 +1,15 @@
+// const score = 100
+// console.log(score);
+
+// const Price = 200
+// console.log(Price.toString().length);
+// console.log(Price.toFixed(1));
+
+// const value = 123.789
+// console.log(value.toPrecision(4));
+
+// const hundreads = 1000000
+// console.log(hundreads.toLocaleString());
+// console.log(hundreads.toLocaleString('en-IN'));
+
+// ------------------------------------- Math ------------------------------------------------------
