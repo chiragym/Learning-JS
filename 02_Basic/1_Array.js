cdc6f762b265ce659957bbc16a3ad83903a7bc00
Chirag Yadav <chirag.cmy@gmail.com>
2025-08-21 06:22:43 +0000

    array

---
+++ 02_Basic/1_Array.js
@@ -0,0 +1,20 @@
+const myArr = [1,2,3,4,5]
+// console.log(myArr[0]); 
+
+// myArr.push(6)
+// console.log(myArr);
+
+// myArr.pop()
+// console.log(myArr);
+// console.log(myArr.length);
+// console.log(myArr.includes(3));
+// console.log(myArr.indexOf(4));
+
+const arr = myArr.join()
+console.log(arr);
+console.log(typeof arr);
+
+
+
+
+
