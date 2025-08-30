9493502013ec08e6156338d45386f0453f49c137
Chirag Yadav <chirag.cmy@gmail.com>
2025-08-30 10:41:47 +0000

    scope finish and intro of hiisting

--- 03_baisc/02_scope.js
+++ 03_baisc/02_scope.js
@@ -1,14 +1,39 @@
 // Inside if condition written code is called local scope and outside code is called global scope
 
 // var c = 300
-let a = 300
-if(true){
-    let a =10
-    const b = 20
-    console.log("Inner is",a);
-}
+// let a = 300
+// if(true){
+//     let a =10
+//     const b = 20
+//     console.log("Inner is",a);
+// }
 
-console.log(a);
+// console.log(a);
 // console.log(b);
 
+//Nested function in this child function can access parent function values
+function one(){
+    const username  = "chirag"
 
+    function two(){
+        const email = "Youtube"
+        console.log(username); 
+    }
+    // console.log(email)
+    two()
+}
+// one()
+
+//Hoisting
+    // if you make one function with simple method you can acces it before initialization but 
+    // if you store funcion in var than you can't access it before initialization
+console.log(addone(5)); 
+function addone(num){
+    return num +1
+}
+
+
+addtwo(5) // Cannot access 'addtwo' before initialization
+const addtwo = function add(val){
+    return val + 2
+}
