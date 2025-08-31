4b61b08c1f3ace1ebe555e99669555cbe2637c9f
Chirag Yadav <chirag.cmy@gmail.com>
2025-08-31 10:50:50 +0000

    if else, switch, tuuthy and falst values

---
+++ 04_controlflow/one.js
@@ -0,0 +1,19 @@
+// if 
+
+// const temprature = 45
+
+// if (temprature === 45) {
+//     console.log("temprature is less than 50");
+    
+// } else {
+//     console.log("temprature is greather than 50");
+    
+// }
+// >,<,<=,>=,!=,==,===,!==
+
+const userloggedIn = true
+const debitcard = true
+if (userloggedIn && debitcard) {
+    console.log("Allow for shoping");  
+}
+
---
+++ 04_controlflow/truthy.js
@@ -0,0 +1,25 @@
+// const userEmail = "C@chirag.ai"
+// if (userEmail) {
+//     console.log("Got user email"); 
+// }else{
+//     console.log("Don't have user email");
+// }
+
+// falsy values
+// false, 0, -0, BigInt 0n, null, undefined, "", NaN
+
+// truthy values
+// "0", "false", [], {}, function () {}, " "
+
+// Nullish coaleashing Operator (??): null, undefined
+
+// let val1;
+// val1 = 5 ?? 10
+// val1 = null ?? 10
+// val1 = undefined ?? 15
+// val1 = null ?? 10 ?? 10
+// console.log(val1);
+
+// Ternary operator
+
+// condition ? true : false
