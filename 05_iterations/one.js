5827ea69941c93a81c6ec31d7aa960d1c3df0463
Chirag Yadav <chirag.cmy@gmail.com>
2025-09-01 09:07:45 +0000

    whle , do while

--- 05_iterations/one.js
+++ 05_iterations/one.js
@@ -1,17 +1,35 @@
 // for
 
-for (let i = 0; i < 10; i++) {
-    const element = i;
-    // console.log(element);  
-}
+// for (let i = 0; i < 10; i++) {
+//     const element = i;
+//     // console.log(element);  
+// }
 
-for (let i = 1; i <= 10; i++) {
-    // console.log(`Outer Loop value:${i}`);
+// for (let i = 1; i <= 10; i++) {
+//     // console.log(`Outer Loop value:${i}`);
     
-    for (let j = 0; j <= 10; j++) {
-        // console.log(i + "X" + j + "=" + i*j );
-    }
-}
+//     for (let j = 0; j <= 10; j++) {
+//         // console.log(i + "X" + j + "=" + i*j );
+//     }
+// }
 
+// ************ While and Do-While loop ************************
+// let index = 0
+// while (index <= 10) {
+//     console.log(`Value of index is ${index}`);
+//     index = index + 2  
+// }
 
+// let superhero = ["Superman","Batman","Shaktiman"]
+// let arr = 0
+// while (arr < superhero.length) {
+//     console.log(`Super Heroes are : ${superhero[arr]}`);
+//     arr = arr + 1  
+// }
+
+// let score = 1
+// do {
+//     console.log(`Score is ${score}`);
+//     score++
+// } while (score <= 50);
 
